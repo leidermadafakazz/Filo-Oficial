9bfa8964c8abb80bccbc38e76b73ed0189e6fb9f
leidermadafakazz <tajo.musica@gmail.com>
2026-01-28 20:14:25 -0500

    Agregamos scrip para el usuario

---
+++ src/Scripts/login.js
@@ -0,0 +1,9 @@
+document.getElementById("userBtn").addEventListener("click", () => {
+    const logueado = false;
+
+    if (logueado) {
+        window.location.href = "src/components/UserCall.astro";
+    } else {
+        window.location.href = "src/components/UserCall.astro";
+    }
+});
